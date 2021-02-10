e6fde98b53bf3b2a14e1498fbb0e39d31b216f63
luke-wilde <lukewilde95@gmail.com>
2021-02-10 14:10:40 -0500

    Hi am back two

--- Rscript.R
+++ Rscript.R
@@ -1,2 +1,3 @@
 # HI I AM LUKE I AM A silly boy 
 
+# Hi I am back 
---
+++ convert_kmltoshp.R
@@ -0,0 +1,4 @@
+# Author: Luke Wilde
+# Date: Feb 5 2021
+# Script to read .kml of ECUs from Bob Crabtree into a single .shp and individual ECU .shp's
+
